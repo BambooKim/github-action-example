042a7f69dbdb9f76ed0fb654818991420e328111
BambooKim <rlaqjarn1008@gmail.com>
2022-10-06 18:27:08 +0900

    chore: Spring Boot 프로젝트 추가

---
+++ src/test/java/com/bamboo/action/ActionApplicationTests.java
@@ -0,0 +1,15 @@
+package com.bamboo.action;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
